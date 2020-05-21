ed551fe24901125e3787711ae9402626d25d8e5f
Jake <jake.dombnz@gmail.com>
2020-05-21 15:03:58 +1200

    modified:   main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def makeClearBoard():
                 board[x][y].append([])
                 for z in range(SIZE):
                     #print("{} {} {} {}".format(x,y,w,z))
-                    square = 0
+                    square = "_"
                     board[x][y][w].append(square)
 					
 def textBoard():
@@ -35,7 +35,22 @@ def textBoard():
             print(line)
     print("-----------------------------------------------------")
 
-def distanceFrom
+def distanceFrom(s1, s2):
+    dist = abs(s1[0] - s2[0]) + abs(s1[1] - s2[1]) + abs(s1[2] - s2[2]) + abs(s1[3] - s2[3])
+    return dist
+
+def testFunc():
+    global board
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            for w in range(len(board[0][0])):
+                for z in range(len(board[0][0][0])):
+                    #board[x][y][w][z] = distanceFrom((x, y, w, z), (1, 1, 1, 1))
+                    if abs(x - 1) <= 1 and abs(y - 1) <= 1 and abs(w - 1) <= 1 and abs(z - 1) <= 1:
+                        board[x][y][w][z] = 1
+                        if distanceFrom((x, y, w, z), (1, 1, 1, 1)) == 0:
+                            board[x][y][w][z] = "K"
 
 makeClearBoard()
+testFunc()
 textBoard()
